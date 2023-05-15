9c054b14078ac1a06539ed74d133445a825fffa5
ThaiBenjamin <25bthai52@gmail.com>
2023-05-15 13:56:29 -0700

    casdf

--- app/src/main/java/com/example/foodfit/HomeActivity.java
+++ app/src/main/java/com/example/foodfit/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.foodfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void goalEventRunner(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
