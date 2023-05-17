921990e18c70a0945d633d4587d8b0f169a67b57
ntnx0246 <shawngazin@protonmail.com>
2023-05-17 04:53:02 -0700

    Added a login system

--- app/src/main/java/com/example/foodfit/HomeActivity.java
+++ app/src/main/java/com/example/foodfit/HomeActivity.java
@@ -5,13 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logoutButton);
+        textView = findViewById(R.id.welcomeText);
+        user = mAuth.getCurrentUser();
+        if(user == null){
+            Intent intent = new Intent(getApplicationContext(), SignInPage.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            textView.setText("Welcome " + user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(getApplicationContext(), SignInPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void goalEventRunner(View view){
--- app/src/main/java/com/example/foodfit/register.java
+++ app/src/main/java/com/example/foodfit/register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -19,13 +20,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignInPage extends AppCompatActivity {
+public class register extends AppCompatActivity {
+
     TextInputEditText editTextEmail, editTextPassword;
-    Button buttonLogin;
+    Button buttonReg;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
 
-    Button buttonCreate;
+    TextView textView;
 
     @Override
     public void onStart() {
@@ -41,62 +43,64 @@ public class SignInPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in_page);
+        setContentView(R.layout.activity_register);
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
-        buttonLogin = findViewById(R.id.btn_login);
+        buttonReg = findViewById(R.id.btn_register);
         progressBar = findViewById(R.id.progressBar);
-        buttonCreate = findViewById(R.id.btn_registerNow);
+        textView = findViewById(R.id.LoginNow);
         mAuth = FirebaseAuth.getInstance();
-        buttonCreate.setOnClickListener(new View.OnClickListener() {
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), register.class);
+                Intent intent = new Intent(getApplicationContext(), SignInPage.class);
                 startActivity(intent);
                 finish();
             }
         });
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
                 String email = editTextEmail.getText().toString();
                 String password = editTextPassword.getText().toString();
 
-
                 if (TextUtils.isEmpty(email)) {
                     editTextEmail.setError("Email is Required.");
                     return;
                 }
-
 
                 if (TextUtils.isEmpty(password)) {
                     editTextPassword.setError("Password is Required.");
                     return;
                 }
 
-                mAuth.signInWithEmailAndPassword(email, password)
+                if (password.length() < 6) {
+                    editTextPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                // register the user in firebase
+                mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    Toast.makeText(register.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), SignInPage.class);
                                     startActivity(intent);
                                     finish();
-                                    // Sign in success, update UI with the signed-in user's information
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Toast.makeText(SignInPage.this, "Authentication failed.",
+                                    Toast.makeText(register.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
-                        });
-
-
-            }
         });
+    }
+
+    });
     }
 }
